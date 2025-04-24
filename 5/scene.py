db663f1656bd2b42b7d65a7badff2a0421643419
Alex Davidchuk <sasha.d1990@list.ru>
2025-04-24 16:02:12 +0400

    Completed 5 lab

--- 5/scene.py
+++ 5/scene.py
@@ -32,12 +32,20 @@ class Scene:
 
         del objModels[i]
         
-    def addModel(self, objModel : ObjModel):
+    def addModel(self, scale, objModel : ObjModel):
         self.objModels.append(objModel)
 
-    def rotateModel(self, degrees, i : int):
-        a = {k: radians(v) for k,v in degrees.items()}
-        
+        #self.objModels[len(self.objModels)-1].v[:,0] += ( self.width / 2 ) / scale
+        #self.objModels[len(self.objModels)-1].v[:,1] += ( self.height / 2 ) / scale
+
+    def rotateModelEuler(self, degrees, i : int, type='degree'):
+        if type=='degree':
+            a = {k: radians(v) for k,v in degrees.items()}
+        elif type=='radian':
+            a = degrees
+        else:
+            raise ValueError("Неизвестный тип")
+            
         X_rotate = np.array([[1, 0, 0], [0, cos(a['x']), -sin(a['x'])], [0, sin(a['x']), cos(a['x'])]])
         Y_rotate = np.array([[cos(a['y']), 0, sin(a['y'])], [0, 1, 0], [-sin(a['y']), 0, cos(a['y'])]])
         Z_rotate = np.array([[cos(a['z']), -sin(a['z']), 0], [sin(a['z']), cos(a['z']), 0], [0, 0, 1]])
@@ -48,30 +56,71 @@ class Scene:
         self.objModels[i].fn = (R @ self.objModels[i].fn.T).T
         self.objModels[i].vn = (R @ self.objModels[i].vn.T).T
 
-    def scaleModel(self, init_scale, i : int):
-        z_shift = 2.0 * abs(self.objModels[i].v[:, 2].min())
-        scale = init_scale * z_shift
+    def rotateModelQuaternion(self, degree, vector, i : int, type='degree'):
+        vector /= np.linalg.norm(vector)
 
-        self.objModels[i].v[:, 0] = scale * self.objModels[i].v[:, 0] / (self.objModels[i].v[:, 2] + z_shift) + self.height / 2
-        self.objModels[i].v[:, 1] = scale * self.objModels[i].v[:, 1] / (self.objModels[i].v[:, 2] + z_shift) + self.width / 2
-        self.objModels[i].v[:, 2] *= init_scale
+        if type=='degree':
+            radian = radians(degree / 2)
+        elif type=='radian':
+            radian = degree
+        else:
+            raise ValueError("Неизвестный тип")
+            
+        a = cos(radian)
+        b = vector[0] * sin(radian)
+        c = vector[1] * sin(radian)
+        d = vector[2] * sin(radian)
+        R = np.array([
+            [a*a + b*b - c*c - d*d, 2*b*c - 2*a*d, 2*b*d + 2*a*c],
+            [2*b*c + 2*a*d, a*a - b*b + c*c - d*d, 2*c*d - 2*a*b],
+            [2*b*d - 2*a*c, 2*c*d + 2*a*b, a*a - b*b - c*c + d*d]
+        ])
+
+        self.objModels[i].v = (R @ self.objModels[i].v.T).T
+        self.objModels[i].fn = (R @ self.objModels[i].fn.T).T
+        self.objModels[i].vn = (R @ self.objModels[i].vn.T).T
+        
+
+    def scaleModel(self, init_scale, i : int):
+        self.objModels[i].v *= init_scale
 
     def shiftModel(self, model_shift, i : int):
         self.objModels[i].v += model_shift
+
+    def perspectiveTransformModel(self, camera_shift_z, init_scale, i : int):
+        z_shift = 1.1*abs(self.objModels[i].v[:, 2].min())
+
+        self.objModels[i].v[:, 2] += z_shift
+        self.objModels[i].v[:, 2] /= init_scale
+        self.objModels[i].v[:, 2] += camera_shift_z     
+        
+        self.objModels[i].v[:, 0] = self.objModels[i].v[:, 0] / (self.objModels[i].v[:, 2]) + self.width / 2
+        self.objModels[i].v[:, 1] = self.objModels[i].v[:, 1] / (self.objModels[i].v[:, 2]) + self.height / 2
     
     def renderScene(self, filename : str):
+        
         for model in self.objModels:
             i = 0
             for face in model.f:
-                norm = model.fn[i]
-                angle = np.dot(norm, self.camera)/np.linalg.norm(norm)
-                if (angle < 0):
-                    self.draw_triangle(model, face)
+                dividedFaces = []
+                numberOfDots = len(face[:, 0])
+                for dot in range(2, numberOfDots):
+                    newFace = []
+                    newFace.append(face[0])
+                    newFace.append(face[dot-1])
+                    newFace.append(face[dot])
+                    dividedFaces.append(np.array(newFace))
+
+                for dividedFace in dividedFaces:
+                    norm = model.fn[i]
+                    angle = np.dot(norm, self.camera)/np.linalg.norm(norm)
+                    if (angle < 0):
+                        self.draw_triangle(model, dividedFace)
                 i += 1    
         Image.fromarray(np.rot90(self.matrix, 2), 'RGB').save(filename)
 
     def draw_triangle(self, model, face):
-        polygon = face[:, 0]
+        polygon = face[:, 0]      
         normals = face[:, 2]
         
         y = np.array([model.v[polygon[i]][0] for i in range(len(polygon))])
